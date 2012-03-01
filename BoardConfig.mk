920da45b78511125fadd3f87fa59e11b62f0f7f8
freexperia <jerpelea@gmail.com>
2012-03-01 17:15:57 +0200

    semc: enabled sensors from source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,7 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/coconut/BoardConfigVendor.mk
 
-BUILD_SEMC_SENSORS := false
+BUILD_SEMC_SENSORS := true
 SENSORS_COMPASS_AK897X := true
 SENSORS_ACCEL_BMA150_INPUT := false
 SENSORS_ACCEL_BMA250_INPUT := true
