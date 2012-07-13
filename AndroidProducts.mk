552d85595de4020043e71cc66805de6b555b0859
freexperia <jerpelea@gmail.com>
2012-07-13 20:49:18 +0300

    coconut: update inclusion

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -26,4 +26,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device_coconut.mk
+    $(LOCAL_DIR)/full_coconut.mk
--- cm.mk
+++ cm.mk
@@ -1,22 +1,20 @@
-## Specify phone tech before including full_phone
+# Inherit device configuration
+$(call inherit-product, device/semc/coconut/full_coconut.mk)
+
+# Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := WT19i
-
-TARGET_BOOTANIMATION_NAME := vertical-320x480
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/semc/coconut/device_coconut.mk)
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := coconut
-PRODUCT_NAME := cm_coconut
-PRODUCT_BRAND := SEMC
-PRODUCT_MODEL := Xperia Live
+# Release name
+PRODUCT_RELEASE_NAME := WT19i
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=WT19i BUILD_FINGERPRINT="SEMC/WT19i_1254-7338/WT19i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys" PRIVATE_BUILD_DESC="WT19i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
+
+TARGET_BOOTANIMATION_NAME := vertical-320x480
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := cm_coconut
+PRODUCT_DEVICE := coconut
