ef3ecdb0a69ebc61a69b44775969d41afccd0270
freexperia <jerpelea@gmail.com>
2012-09-16 17:28:45 +0300

    coconut: fix device name for app compatibility check

--- cm.mk
+++ cm.mk
@@ -11,7 +11,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_RELEASE_NAME := WT19i
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=WT19i BUILD_FINGERPRINT="SEMC/WT19i_1254-7338/WT19i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys" PRIVATE_BUILD_DESC="WT19i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_BRAND="Sony Ericsson" PRODUCT_NAME="Live with Walkman" BUILD_FINGERPRINT="SEMC/WT19i_1254-7338/WT19i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys" PRIVATE_BUILD_DESC="WT19i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
 
 TARGET_BOOTANIMATION_NAME := vertical-320x480
 
